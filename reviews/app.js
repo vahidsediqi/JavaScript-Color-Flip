5dc1ca9013a05214213f5cbd4faa16b11d0030b3
vahidsediqi <vsediqi@live.com>
2022-01-13 16:18:02 +0300

    data pased

--- reviews/app.js
+++ reviews/app.js
@@ -1,44 +1,51 @@
-// local reviews data
-const reviews = [
-  {
-    id: 1,
-    name: "Neha Karkar",
-    job: "Bollywood Singer",
-    img:
-      "https://i.scdn.co/image/ab6761610000e5eb2eb3676d6d03d8872feb0875",
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-  },
-  {
-    id: 2,
-    name: "anna johnson",
-    job: "web designer",
-    img:
-      "https://kirunews.com/wp-content/uploads/2019/12/GG.jpg",
-    text:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-  },
-  {
-    id: 3,
-    name: "Johnny Depp",
-    job: "Bollywood Actor",
-    img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStpy5AJ7T5ogRTjgXmakPRf0SaxtG5fA-7YtfI7aqZKjBCSi7R",
-    text:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-  },
-  {
-    id: 4,
-    name: "Keanu Reeves",
-    job: "Hollywood Star",
-    img:
-      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTkqugX0WLc78TSUXjzYAQvwT7nqU8vJknuJGldrNv0FO7kD8vl",
-    text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-  },
-];
+// // local reviews data
+// const reviews = [
+//   {
+//     id: 1,
+//     name: "Neha Karkar",
+//     job: "Bollywood Singer",
+//     img:
+//       "https://i.scdn.co/image/ab6761610000e5eb2eb3676d6d03d8872feb0875",
+//     text:
+//       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+//   },
+//   {
+//     id: 2,
+//     name: "anna johnson",
+//     job: "web designer",
+//     img:
+//       "https://kirunews.com/wp-content/uploads/2019/12/GG.jpg",
+//     text:
+//       "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+//   },
+//   {
+//     id: 3,
+//     name: "Johnny Depp",
+//     job: "Bollywood Actor",
+//     img:
+//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStpy5AJ7T5ogRTjgXmakPRf0SaxtG5fA-7YtfI7aqZKjBCSi7R",
+//     text:
+//       "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+//   },
+//   {
+//     id: 4,
+//     name: "Keanu Reeves",
+//     job: "Hollywood Star",
+//     img:
+//       "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTkqugX0WLc78TSUXjzYAQvwT7nqU8vJknuJGldrNv0FO7kD8vl",
+//     text:
+//       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+//   },
+// ];
 
-// selecting items
+
+const dataURL = ('https://vahidsediqi.github.io/Json-data/review-app/data.json')
+
+async function getData() {
+  const data = await fetch(dataURL);
+  const reviews = await data.json();
+  console.log(reviews)
+  // selecting items
 const img = document.getElementById('person-img');
 const author = document.getElementById('author');
 const job = document.getElementById('job');
@@ -48,17 +55,14 @@ const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 const randomBtn = document.querySelector('.random-btn');
 
-// setting starting item
 
 let currentItem = 0;
 
-// load intial item
 
 window.addEventListener('DOMContentLoaded', () => {
   showPerson(currentItem)
 })
 
-// Show Person Based On Item
 const showPerson = (person) => {
   const item = reviews[person]
   img.src = item.img
@@ -76,7 +80,9 @@ nextBtn.addEventListener('click', () => {
   showPerson(currentItem)
 })
 
-// Show Previews Person
 prevBtn.addEventListener('click', () => {
   
 })
+}
+
+getData()
