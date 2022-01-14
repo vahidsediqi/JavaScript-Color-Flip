25db29085c4bd77ebf6eb503511264e892fd3125
vahidsediqi <vsediqi@live.com>
2022-01-14 11:03:23 +0300

    data fetched from api

--- reviews/app.js
+++ reviews/app.js
@@ -41,48 +41,62 @@
 
 const dataURL = ('https://vahidsediqi.github.io/Json-data/review-app/data.json')
 
-async function getData() {
-  const data = await fetch(dataURL);
-  const reviews = await data.json();
-  console.log(reviews)
-  // selecting items
 const img = document.getElementById('person-img');
 const author = document.getElementById('author');
 const job = document.getElementById('job');
 const info = document.getElementById('info');
-
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 const randomBtn = document.querySelector('.random-btn');
-
-
 let currentItem = 0;
 
+async function getData() {
+  const data = await fetch(dataURL);
+  const reviews = await data.json();
+    img.src = reviews[currentItem].img
+    author.textContent = reviews[currentItem].name;
+    job.textContent = reviews[currentItem].job;
+    info.textContent = reviews[currentItem].text;
 
-window.addEventListener('DOMContentLoaded', () => {
-  showPerson(currentItem)
-})
+    // add eventListner
+    nextBtn.addEventListener('click', ()=> {
+      currentItem ++
+      if(currentItem >  reviews.length - 1) {
+        currentItem = 0
+      } 
+        img.src = reviews[currentItem].img
+        author.textContent = reviews[currentItem].name;
+        job.textContent = reviews[currentItem].job;
+        info.textContent = reviews[currentItem].text;
+      
+    });
 
-const showPerson = (person) => {
-  const item = reviews[person]
-  img.src = item.img
-  author.textContent = item.name
-  job.textContent = item.job
-  info.textContent = item.text
+    prevBtn.addEventListener('click',() => {
+       if(currentItem >= 1) {
+         currentItem --;
+         img.src = reviews[currentItem].img
+         author.textContent = reviews[currentItem].name;
+         job.textContent = reviews[currentItem].job;
+         info.textContent = reviews[currentItem].text;
+       } else {
+        currentItem = reviews.length - 1;
+        img.src = reviews[currentItem].img
+        author.textContent = reviews[currentItem].name;
+        job.textContent = reviews[currentItem].job;
+        info.textContent = reviews[currentItem].text;
+       }
+    } 
+    )
+
+    randomBtn.addEventListener('click', () => {
+      const randNumber = Math.floor(Math.random() * reviews.length)
+      currentItem = randNumber;
+      img.src = reviews[currentItem].img
+      author.textContent = reviews[currentItem].name;
+      job.textContent = reviews[currentItem].job;
+      info.textContent = reviews[currentItem].text;
+    })
 }
 
-nextBtn.addEventListener('click', () => {
-  currentItem ++;
 
-  if(currentItem > reviews.length - 1) {
-    currentItem = 0;
-  }
-  showPerson(currentItem)
-})
-
-prevBtn.addEventListener('click', () => {
-  
-})
-}
-
-getData()
+getData();
